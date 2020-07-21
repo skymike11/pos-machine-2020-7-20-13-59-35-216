33e5a0ed2e2b4be9907aa7f7d10feb33437e83bf
skymike <skymike11@qq.com>
2020-07-21 22:11:32 +0800

    commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
+# PDCA
+
+|      | 统计每个item有多少                                           |
+| ---- | ------------------------------------------------------------ |
+| P    | 60min                                                        |
+| D    | 80min                                                        |
+| C    | 1. 期初在代码一些位置写的不够严谨，有一点bug，在调试程序上花费了一些时间             2. 在画图的时候和老师的做法换了一种思路，重新调整了一下流程结构 |
+| A    | 继续熟悉一下vscode的debug界面                                |
+
+
+
 # Requirement
+
 A cash register (POS) system is used in the store for settlement of the store. This cashier will settle and print the receipt (Receipt) according to the item(Item) in the customer's shopping cart (Cart) at the time of settlement.
 
 We need to implement a function called printReceipt, which can input the data of the specified format as a parameter and then output the text of the receipt in the browser console.
--- main.js
+++ main.js
@@ -1,15 +1,115 @@
-function printReceipt(barcodes) {
-    console.log(`
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************`)
+const products = [
+    {
+        barcode: 'ITEM000000',
+        name: 'Coca-Cola',
+        price: 3
+    },
+    {
+        barcode: 'ITEM000001',
+        name: 'Sprite',
+        price: 3
+    },
+    {
+        barcode: 'ITEM000002',
+        name: 'Apple',
+        price: 5
+    },
+    {
+        barcode: 'ITEM000003',
+        name: 'Litchi',
+        price: 15
+    },
+    {
+        barcode: 'ITEM000004',
+        name: 'Battery',
+        price: 2
+    },
+    {
+        barcode: 'ITEM000005',
+        name: 'Instant Noodles',
+        price: 4
+    }
+]
+
+const productsMap = initMapData(products)
+
+function initMapData (products) {
+    let map = new Map()
+    products.forEach(product => {
+        map.set(product.barcode, product)
+    })
+    return map
 }
 
+function printReceipt(barcodes) {
+    let barcodeNumberMap = createBarcodeNumMap(barcodes)
+    let itemList = createItemList(barcodeNumberMap)
+    let totalPirce = countTotalPrice(itemList)
+    console.log(formatDetail(itemList, totalPirce))
+}
 
+function createBarcodeNumMap(barcodes) {
+    let barcodeNumberMap = new Map()
+    barcodes.forEach(barcode => {
+        if (barcodeNumberMap.get(barcode) === undefined) {
+            let product = findProductByCode(barcode)
+            barcodeNumberMap.set(barcode, 1)
+        } else {
+            let currentNum = barcodeNumberMap.get(barcode)
+            barcodeNumberMap.set(barcode, currentNum + 1)
+        }
+    })
+    return barcodeNumberMap
+}
+
+function findProductByCode(barcode) {
+    return productsMap.get(barcode)
+}
+
+function createItemList(barcodeNumberMap) {
+    let itemList = []
+    barcodeNumberMap.forEach((number, barcode)=> {
+        let product = productsMap.get(barcode)
+        itemList.push({
+            'itemName' : product.name,
+            'quantity' : number,
+            'unitPrice' : product.price
+        })
+    })
+    return itemList
+}
+
+function countTotalPrice(itemList) {
+    let totalPrice = 0.0
+    itemList.forEach(item => {
+        totalPrice += item.unitPrice * item.quantity
+    })
+    return totalPrice
+}
+
+function formatDetail (itemList, totalPrice) {
+    let detailInfo = ''
+    detailInfo += '\n***<store earning no money>Receipt ***\n'
+    itemList.forEach(item => {
+      detailInfo +=  'Name: ' + item.itemName + ', Quantity: ' 
+      + item.quantity + ', Unit price: ' + item.unitPrice +' (yuan), Subtotal: ' + (item.quantity * item.unitPrice) + ' (yuan)' + '\n'
+    })
+    detailInfo += '----------------------\n'
+    detailInfo += 'Total: ' + totalPrice + ' (yuan)'
+    detailInfo += '\n' + '**********************'
+    return detailInfo
+}
+// const barcodes = [
+//     'ITEM000000',
+//     'ITEM000000',
+//     'ITEM000000',
+//     'ITEM000000',
+//     'ITEM000000',
+//     'ITEM000001',
+//     'ITEM000001',
+//     'ITEM000004'
+// ]
+// printReceipt(barcodes)
 module.exports = {
     printReceipt
 };
